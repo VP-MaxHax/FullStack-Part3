42046e24f4a07e6942888e576299a9d69b4bccd1
VP-MaxHax <vp.juselius@gmail.com>
2024-07-14 13:07:34 +0300

    uibuild

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config()
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
@@ -10,127 +10,127 @@ app.use(cors())
 app.use(express.static('dist'))
 
 app.use((req, res, next) => {
-  const oldSend = res.send;
+  const oldSend = res.send
   res.send = function(data) {
-    res.locals.body = data;
-    oldSend.apply(res, arguments);
+    res.locals.body = data
+    oldSend.apply(res, arguments)
   }
-  next();
-});
+  next()
+})
 
-morgan.token('body', (req, res) => res.locals.body);
+morgan.token('body', (req, res) => res.locals.body)
 
 app.use(express.json())
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-  app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-      response.json(persons)
-    })
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
   })
+})
 
-  app.get('/info', (request, response) => {
-    const date = new Date()
-    Person.find({}).then(persons => {
-      response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
-    })
+app.get('/info', (request, response) => {
+  const date = new Date()
+  Person.find({}).then(persons => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
   })
+})
 
-  app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-      .then(person => {
-        if (person) {
-          response.json(person)
-        } else {
-          response.status(404).end()
-        }
-      })
-      .catch(error => next(error))
-  })
-
-  app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
-  })
-
-  app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-  
-    const person = {
-      name: body.name,
-      number: body.number,
-    }
-  
-    Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
-      .then(updatedPerson => {
-        response.json(updatedPerson)
-      })
-      .catch(error => next(error))
-  })
-
-  const generateId = () => {
-    let id = Math.floor(Math.random() * 1000000);
-    return String(id);
-  }
-  
-  app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-    console.log('body', body);
-  
-    if (!body.name) {
-      return response.status(400).json({ 
-        error: 'name missing' 
-      })
-    }
-
-    if (!body.number) {
-      return response.status(400).json({ 
-        error: 'number missing' 
-      })
-    }
-
-    const person = new Person({
-      name: body.name,
-      number: body.number,
-      id: generateId(),
-    })
-
-    console.log('person', person);
-  
-    person.save().then(savedPerson => {
-      console.log('savedPerson', savedPerson);
-      response.json(savedPerson)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
     .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const generateId = () => {
+  let id = Math.floor(Math.random() * 1000000)
+  return String(id)
+}
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+  console.log('body', body)
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+    id: generateId(),
   })
 
-  const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
-  // olemattomien osoitteiden käsittely
-  app.use(unknownEndpoint)
+  console.log('person', person)
 
-  const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-      return response.status(400).json({ error: error.message })
-    }
-  
-    next(error)
-  }
-  
-  // tämä tulee kaikkien muiden middlewarejen ja routejen rekisteröinnin jälkeen!
-  app.use(errorHandler)
-
-    const PORT = process.env.PORT || 3001
-    app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  person.save().then(savedPerson => {
+    console.log('savedPerson', savedPerson)
+    response.json(savedPerson)
   })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// tämä tulee kaikkien muiden middlewarejen ja routejen rekisteröinnin jälkeen!
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
