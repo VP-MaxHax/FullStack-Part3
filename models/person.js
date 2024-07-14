26f5015a628377b5c17ee854fa473a71ecdc11e2
VP-MaxHax <vp.juselius@gmail.com>
2024-07-14 12:20:41 +0300

    uibuild

--- models/person.js
+++ models/person.js
@@ -22,7 +22,12 @@ mongoose.connect(url)
     },
     number: {
       type: String,
-      match: /^\d{2,3}-\d{5,}$/,
+      validate: {
+        validator: function(v) {
+        return /^\d{2,3}-\d{5,}$/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number! Must be xx-xxxxx or xxx-xxxxx`
+      },
       required: true
     }
   })
